a4b278e166787d13d5163ff84b852373b635ff0b
Justin Bare <jbare7@gmail.com>
2024-11-10 17:02:30 -0800

    basic Spring controller is working

---
+++ src/main/java/app/CampaignerApplication.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampaignerApplication.class, args);
+    }
+}
